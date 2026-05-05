83981f39f854423f655c3344dd0c66c9014e9cf6
JamesA1ves <108427767+JamesA1ves@users.noreply.github.com>
2026-05-05 13:48:03 -0300

    Primeiro upload do projeto

---
+++ Projeto/codigo.py
@@ -0,0 +1,43 @@
+import pyautogui
+import pandas as pd
+import time
+
+tabela = pd.read_csv("produtos.csv")
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+time.sleep(3)
+
+pyautogui.click(1420, y=847)
+pyautogui.write("emaildousuario@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("senhausuario")
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+for linha in tabela.index:
+    pyautogui.click(x=1359, y=587)
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    if not pd.isna(tabela.loc[linha, "obs"]):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+        
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
